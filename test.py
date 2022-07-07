1b5be5db004033303ed4d39e3e6895b07f0a697e
sivalokeshyadav <222sivalokeshyadav@gmail.com>
2022-07-07 15:36:31 +0530

    -i have updated the code at line number 312 - ihave did few more changes in xyz.py - all the updates are done for this version:-

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from app import Calculator
 cal = Calculator(a=3,b=4)
 print(f"Adding the values :-{cal.addition()}")    
-print(f"Substracting the values :-{cal.addition()}")    
+print(f"Substracting the values :-{cal.substraction()}")    
+print(f"multiplyinging the values :-{cal.multiplication()}") 
